c84aae4e994dd7583d8ec2c5c0cdf386fd2e614f
Alok Kumar <alok.kumar@ovyo.com>
2025-04-11 13:02:54 +0530

    fixing race condition using GCD

--- Sources/ALKAsyncNetwork/ALKAsyncNetwork.swift
+++ Sources/ALKAsyncNetwork/ALKAsyncNetwork.swift
@@ -34,12 +34,23 @@ public class ALKNetworkManager: ALKNetwork {
     
     private var config: NetworkConfig?
     
+    // Private serial queue to protect config
+    private let queue = DispatchQueue(label: "com.alk.network.queue")
+    
     public init(config: NetworkConfig? = nil) {
         self.config = config
     }
     
+    private func getConfig() -> NetworkConfig? {
+        var result: NetworkConfig?
+        queue.sync {
+            result = self.config
+        }
+        return result
+    }
+    
     public func request<T: Decodable>(_ type: T.Type, request: Request) async throws -> T {
-        guard let config = config else {
+        guard let config = getConfig() else {
             throw ALKNetworkError.configMissing
         }
         
